523f02c0c6f68792d9158e841a9705282401701c
stumpf-alexandre <als.stumpf@gmail.com>
2022-09-07 23:48:25 -0300

    criando variaveis de calculos

--- variaveis/variaveis.java
+++ variaveis/variaveis.java
@@ -5,5 +5,23 @@ public class variaveis {
         
         String nome = "Alexandre";
         System.out.println("Olá, " + nome);
+
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int sub = a - b;
+        int mult = a * b;
+        int div1 = a / b;
+        float div2 = a / b;
+        //cast
+        float div3 = (float) a / b;
+
+        System.out.println(soma);
+        System.out.println(sub);
+        System.out.println(mult);
+        System.out.println(div1);
+        System.out.println(div2);
+        System.out.println(div3);
     }    
 }
