646664d4dd3adb4cd6c709a81143d5c40a7b81c5
stumpf-alexandre <als.stumpf@gmail.com>
2022-09-07 23:33:05 -0300

    criando variaveis com java no VS code

---
+++ variaveis/variaveis.java
@@ -0,0 +1,9 @@
+package variaveis;
+
+public class variaveis {
+    public static void main(String[] args) {
+        
+        String nome = "Alexandre";
+        System.out.println("Olá, " + nome);
+    }    
+}
